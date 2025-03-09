db07262ea2ea39a09eb2d4ad443aa90803eb7fb8
thakksht <akshatiscoding@gmail.com>
2025-03-09 11:38:52 +0530

    chatbot designing

---
+++ chat/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ChatbotIcon from './components/ChatbotIcon'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">
+              Chatbot
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
